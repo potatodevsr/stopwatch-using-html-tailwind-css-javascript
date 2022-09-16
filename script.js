72d4916adde76799c0fe727a9a0fdb884392337e
potatodevsr <hoshiku1997@gmail.com>
2022-09-16 15:18:23 +0700

    stopWatch

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const stopWatch = document.getElementById("stopWatch");
+    const startBtn = document.getElementById("start");
+    const stopBtn = document.getElementById("stop");
+    const resetBtn = document.getElementById("reset");
+
+    let startTimer = null;
+
+    let [hours, minutes, seconds] = [0, 0, 0];
+
+    function disabled(...elems) {
+        elems.forEach((elem) => elem.setAttribute("disabled", "true"));
+    }
+
+    function unDisabled(...elems) {
+        elems.forEach((elem) => elem.removeAttribute("disabled"));
+    }
+
+    function displayTime() {
+        seconds++;
+
+        if (seconds >= 60) {
+            minutes++;
+            seconds = 0;
+
+            if (minutes >= 60) {
+                hours++;
+                minutes = 0;
+
+                if (hours >= 24) {
+                    hours = 0;
+                }
+            }
+        }
+    }
+})
