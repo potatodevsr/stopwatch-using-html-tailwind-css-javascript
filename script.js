bd82b17b9791cf9987fbf4d9657d6bab509c0028
potatodevsr <hoshiku1997@gmail.com>
2022-09-16 16:31:40 +0700

    stopwatch2

--- script.js
+++ script.js
@@ -32,5 +32,34 @@ window.addEventListener("DOMContentLoaded", function () {
                 }
             }
         }
+
+        let h = hours < 10 ? "0" + hours : hours;
+        let m = minutes < 10 ? "0" + minutes : minutes;
+        let s = seconds < 10 ? "0" + seconds : seconds;
+
+        stopWatch.innerHTML = `${h} : ${m} : ${s}`;
     }
-})
+
+    startBtn.addEventListener("click", () => {
+        if (startTimer !== null) {
+            clearInterval(startTimer);
+        }
+        startTimer = setInterval(displayTime, 1000);
+        disabled(startBtn);
+        unDisabled(stopBtn, resetBtn);
+    });
+
+    stopBtn.addEventListener("click", function () {
+        clearInterval(startTimer);
+        disabled(stopBtn);
+        unDisabled(startBtn);
+    });
+
+    resetBtn.addEventListener("click", function () {
+        clearInterval(startTimer);
+        disabled(resetBtn, stopBtn);
+        unDisabled(startBtn);
+        [hours, minutes, seconds] = [0, 0, 0];
+        stopWatch.innerHTML = "00 : 00 : 00";
+    });
+});
